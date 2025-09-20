951fa6f5575c4fbb1a9c6cfaaff63fa8538f7d85
Luis Netto <luisnetto.2013@gmail.com>
2025-09-20 13:46:24 -0300

    Atualização 20/09/2025

---
+++ 2785.py
@@ -0,0 +1,13 @@
+def main():
+    camadas = int(input())
+
+
+    matriz = []
+    for _ in range(camadas):
+        linha = [int(x) for x in input().split()]
+        matriz.append(linha)
+
+    
+
+
+main()
